db44205a5fc6151002f54b75890a1fbdab93b856
elDiego <d.rodriguez@vida.com.uy>
2020-09-27 21:41:53 -0300

    123

---
+++ src/locale/locales.js
@@ -0,0 +1,13 @@
+import translationES from './translations/es.json';
+import translationEN from './translations/en.json';
+import translationDE from './translations/de.json';
+
+const locales = {
+
+  es: translationES,
+  en: translationEN,
+  de: translationDE,
+
+};
+
+export default locales;
